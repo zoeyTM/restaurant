e3e45b4532ea165ed1d19fa1e6c4b03601434c71
Alberto Alvarez <albertoealvarezm1992@gmail.com>
2017-01-18 20:57:39 -0500

    reserver POST update

--- app/routing/reserve.js
+++ app/routing/reserve.js
@@ -1,11 +1,15 @@
 var express = require("express");
-var router = express.router();
+var router = express.Router();
 var path = require('path'); 
-
+var data = require('tables.js')
 
 router.get("/reserve", function(req, res) {
   res.sendFile(path.join(__dirname, "../public/reservation.html"));
 });
+
+router.post("/reserve/new", function(req, res){
+	
+}
 
  module.exports = router;
 
