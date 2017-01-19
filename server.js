6eb94a327a9d276e4209b3e44d9247863885f1ee
morgansliman <morgansliman@gmail.com>
2017-01-18 19:45:53 -0500

    basic routing

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
 var PORT = 8000;
 
+var reserve = require('./app/routing/reserve');
+var tables = require('./app/routing/tables');
+
+app.use(reserve);
+app.use(tables);
 
 app.listen(PORT, function () {
-	
+	console.log('Server listening on port:', PORT);
 });
